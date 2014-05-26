d59bc0dccd9e07362ccf344365ceb56f89c6449b
Julian Brackins <julian.brackins@mines.sdsmt.edu>
2014-05-26 17:01:51 -0600

    created loop for reading in emails

--- py_read_email.py
+++ py_read_email.py
@@ -1,15 +1,23 @@
+from __future__ import print_function
 import poplib
 from email import parser
 
-pop_conn = poplib.POP3_SSL('pop.gmail.com')
-pop_conn.user('jandvpi@gmail.com')
-pop_conn.pass_('greatwhitebuffalo')
-#Get messages from server:
-messages = [pop_conn.retr(i) for i in range(1, len(pop_conn.list()[1]) + 1)]
-# Concat message pieces:
-messages = ["\n".join(mssg[1]) for mssg in messages]
-#Parse message intom an email object:
-messages = [parser.Parser().parsestr(mssg) for mssg in messages]
-for message in messages:
-    print message['Subject']
-pop_conn.quit()
+blanks = ""
+
+forever = 1
+while forever:
+    pop_conn = poplib.POP3_SSL('pop.gmail.com')
+    pop_conn.user('jandvpi@gmail.com')
+    pop_conn.pass_('greatwhitebuffalo')
+    #Get messages from server:
+    messages = [pop_conn.retr(i) for i in range(1, len(pop_conn.list()[1]) + 1)]
+    # Concat message pieces:
+    messages = ["\n".join(mssg[1]) for mssg in messages]
+    #Parse message intom an email object:
+    messages = [parser.Parser().parsestr(mssg) for mssg in messages]
+    for message in messages:
+        subj = message['Subject']
+        print (subj)
+        for i in range(20):
+            print (blanks)
+    pop_conn.quit()
